f06c0f60c52e70cbf66ed2b24ff57da82c958b40
dvaaz <darley.azevedo@gmail.com>
2026-06-05 21:01:51 -0300

    first push

--- src/pagamento/pagamento.module.ts
+++ src/pagamento/pagamento.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PagamentoService } from './pagamento.service';
 import { PagamentoController } from './pagamento.controller';
+import { PedidoService } from 'src/pedido/pedido.service';
+import { StatusPagamentoService } from 'src/status_pagamento/status_pagamento.service';
+import { PrismaService } from 'src/database/prisma/prisma.service';
 
 @Module({
   controllers: [PagamentoController],
-  providers: [PagamentoService],
+  providers: [
+    PagamentoService,
+    PrismaService,
+    StatusPagamentoService,
+    PedidoService
+  ],
 })
 export class PagamentoModule {}
